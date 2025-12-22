576e9e18c747225be671f64c839aec3fb357bcdc
Roy <roy@cloudsurfer.dev>
2025-12-22 21:40:32 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {MacFixer} from "./components/MacFixer.tsx";
+
+function App() {
+
+  return (
+    <>
+        <MacFixer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MacFixer.tsx
@@ -0,0 +1,35 @@
+import {useState} from "react";
+import * as React from "react";
+
+export const MacFixer = () => {
+    const [input] = useState<string>()
+    const [output, setOutput] = useState<string[]>()
+
+    const handlePaste = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        if (e.target.value === '') {
+            setOutput([])
+            return
+        }
+
+        const output = e.target.value
+            .trim()
+            .split('\n')
+            .filter((mac: string) => mac.length === 12)
+            .map((mac: string) => mac.toUpperCase().replace(/(.{2})(?=.)/g, '$1:'))
+
+        setOutput(output);
+    }
+
+    const copyToClipboard = () => {
+        if (!output) return
+        navigator.clipboard.writeText(output.join('\n'))
+    }
+    return (
+    <>
+        <button onClick={copyToClipboard}>Copy</button>
+        <textarea onChange={handlePaste} value={input} rows={10} className=""/>
+        <textarea value={output?.join('\n')} rows={10} className="" readOnly/>
+    </>
+  )
+}
+
